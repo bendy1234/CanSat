3004b47db351dbcb3a1c766f3f5413d79c431ef9
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2024-03-22 14:58:47 -0700

    redo data transmition

--- esp32/satellite.ino
+++ esp32/satellite.ino
@@ -3,94 +3,144 @@
 #include <LoRa.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+// #include <TinyGPS.h>
+// #include <HardwareSerial.h>
 
-// VV lora pins VV
+// lora pins
 #define ss 5
 #define rst 14
 #define dio0 2
-// VV BMP 280 pins VV
+
+// BMP 280 pins
 #define BME_SCK 13
 #define BME_MISO 12
-#define BME_MOSI 27
-#define BME_CS 26
-// VV for approx altitude with BMP280 (just incase GPS dosent work) VV
+#define BME_MOSI 11
+#define BME_CS 10
+
+// for approx altitude with BMP280 (just incase GPS dosent work)
 #define SEALEVELPRESSURE_HPA (1017.7)
 
+struct BMEData
+{
+  float temperature;
+  float pressure;
+  float humidity;
+};
+
+// struct GPSData
+// {
+//   float latitude;
+//   float longitude;
+//   long altitude;
+//   long date;
+//   long time;
+// };
+
 Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+// Adafruit_BME280 bme(BME_CS); // hardware SPI
+// Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+// TinyGPS gps;
+// HardwareSerial SerialGPS(0, 1);
 
 unsigned long delayTime;
+uint16_t counter = 0;
 
-int counter = 0;
-
-void setup() {
-  //initialize Serial Monitor
+void setup()
+{
+  // initialize Serial Monitor
   Serial.begin(115200);
   while (!Serial);
   Serial.println("LoRa Sender/BMP 280 test");
 
-    unsigned status;
+  unsigned status;
+  status = bme.begin(0x76);
 
-    status = bme.begin(0x76);
-
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-    }
-  //setup LoRa transceiver module
+  if (!status)
+  {
+    Serial.println("Could not find a valid BME280 sensor!");
+  }
+  // setup LoRa transceiver module
   LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
-  //433E6 for Asia
-  //866E6 for Europe
-  //915E6 for North America
-  while (!LoRa.begin(866E6)) {
+
+  // replace the LoRa.begin(---E-) argument with your location's frequency
+  // 915E6 for North America
+  while (!LoRa.begin(866E6))
+  {
     Serial.println(".");
     delay(500);
   }
-   // Change sync word (0xF3) to match the receiver
+
+  // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
   LoRa.setSyncWord(0xF3);
   Serial.println("LoRa Initializing OK!");
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Sending packet #");
   Serial.print(counter);
-
-  sendValues();
-
+  sendBMEData();
   counter++;
+
+  // Serial.println("Sending packet #");
+  // Serial.print(counter);
+  // sendGPSData();
+  // counter++;
 
   delay(1000);
 }
-void sendValues() {
-    LoRa.beginPacket();
-    LoRa.print("Temp= ");
-    LoRa.print(bme.readTemperature());
-    LoRa.print(" C");
 
-    LoRa.println("Pressr= ");
-    LoRa.print(bme.readPressure() / 100.0F);
-    LoRa.print(" hPa");
+void sendBMEData()
+{
+  // ~Alt
+  // dont need to send this, can be caluated on the ground or later
+  // float atmospheric = readPressure() / 100.0F;
+  // return 44330.0 * (1.0 - pow(atmospheric / seaLevel, 0.1903));
 
-    LoRa.println("~Alt= ");
-    LoRa.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    LoRa.print(" m");
+  BMEData data = {
+    bme.readTemperature(),
+    bme.readPressure(),
+    bme.readHumidity(),
+  };
+  uint8_t buffer[sizeof(BMEData)];
+  memcpy(buffer, &data, sizeof(BMEData));
 
-    LoRa.println(bme.readHumidity());
-    LoRa.print("% Humid");
+  LoRa.beginPacket();
+  // packet number
+  LoRa.write('#');
+  LoRa.write(counter, sizeof(uint16_t));
+  // BME data
+  LoRa.write('B');
+  LoRa.write(buffer, sizeof(BMEData));
 
-    LoRa.println("Packet #");
-    LoRa.print(counter);
-
-    LoRa.println();
-    LoRa.endPacket();
-    
+  LoRa.println();
+  LoRa.endPacket();
 }
+
+// void sendGPSData()
+// {
+//   GPSData data = {
+//     0.0,
+//     0.0,
+//     0,
+//     0,
+//     0
+//   };
+
+//   uint8_t buffer[sizeof(GPSData)];
+//   memcpy(buffer, &data, sizeof(GPSData));
+
+//   // send the gps data
+//   LoRa.beginPacket();
+//   // packet number
+//   LoRa.write('#');
+//   LoRa.write(counter, sizeof(uint16_t));
+//   // GPS data
+//   LoRa.write('G');
+//   LoRa.write(buffer, sizeof(GPSData));
+//   LoRa.endPacket();
+
+// }

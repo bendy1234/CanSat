246a0da5aaa6b0d03275f39bebc5603ac2c925a5
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2024-05-05 17:51:30 -0700

    Update startup.py

--- raspberrypi/startup.py
+++ raspberrypi/startup.py
@@ -8,25 +8,24 @@ pi = pigpio.pi()
 
 folder = os.path.expanduser("~/") +  datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 os.mkdir(folder)
+os.mkdir(f"{folder}/images")
 
 # start taking images
-Popen(["libcamera-still", "--timelap", "900", "-o", f"{folder}/images/i%04d.png", "-v", "0"])
+Popen(["libcamera-still", "--timelap", "900", "-t", "999999999", "-o", f"{folder}/images/i%04d.png", "-v", "0"])
 
 def write_data(file):
     (c, data) = pi.serial_read(handle)
     if c > 0:
         file.write(data.decode())
 
-
 with open(f"{folder}/data.txt", "w", buffering=1) as file:
-    try:
-        # serial connection on GPIO 14 and 15
-        handle = pi.serial_open("/dev/ttyS0", 9600)
 
+    # serial connection on GPIO 14 and 15
+    handle = pi.serial_open("/dev/ttyS0", 9600)
+
+    try:
         while True:
             write_data(file)
     finally:
+        file.flush()
         pi.serial_close(handle)
-
-# In crontab:
-# @reboot python3 ~/startup.py &

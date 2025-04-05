8d258b5ec9248642efb5378284080f321e663ba6
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2025-04-05 16:44:07 -0700

    Update peripherals

--- 2024-25/Sender (CanSat)/LoRa Sender.ino
+++ 2024-25/Sender (CanSat)/LoRa Sender.ino
@@ -2,7 +2,7 @@
 #include <LoRa.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
+#include <Adafruit_BME280.h>
 #include <ESP32Servo.h>
 
 // LoRa
@@ -10,10 +10,16 @@
 #define resetPin 40
 #define irqPin 41
 
-#define servoPin 36
+#define aileronServoPin 38
+// #define servoPin2 39
+#define rudderServoPin 42
+// #define servoPin4 45
 
-Adafruit_BME680 bme;  // Address is changed to 0x76 in `setup()`
-Servo myservo;
+Adafruit_BME280 bme;  // Address is changed to 0x76 in `setup()`
+Servo aileronServo;
+// Servo servo2;
+Servo rudderServo;
+// Servo servo4;
 
 byte msgCount = 0;
 long lastSendTime = 0;
@@ -23,25 +29,22 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  Serial.println("LoRa Duplex with BME680");
+  Serial.println("LoRa Duplex with BME280");
 
-  // Initialize servo
-  myservo.attach(servoPin);
-  myservo.write(90);  // set initial position to 90 degrees
+  // Initialize servos
+  aileronServo.attach(aileronServoPin);
+  rudderServo.attach(rudderServoPin);
+
+  // set initial position to 90 degrees
+  aileronServo.write(90);
+  rudderServo.write(90);
 
   // Initialize BME680 sensor
   Wire.begin(8, 9);
   if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    Serial.println("Could not find a valid BME280 sensor!");
     while (true);
   }
-
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320°C for 150 ms
 
   // override the default CS, reset, and IRQ pins (optional)
   LoRa.setPins(csPin, resetPin, irqPin);
@@ -61,28 +64,47 @@ void loop() {
 
   if (millis() - lastSendTime > interval) {
     // Read sensor data
-    unsigned long endTime = bme.beginReading();
-    if (endTime == 0) {
-      Serial.println("Failed to begin reading :(");
-      return;
-    }
-
-    if (!bme.endReading()) {
-      Serial.println("Failed to complete reading :(");
-      return;
-    }
-
+    
     // Format sensor data into a single message
     String msg = String(millis() / 1000.0, 1) + "," + 
-                  String(bme.temperature, 1) + "," + 
-                  String(bme.pressure / 100.0, 1) + "," + 
-                  String(bme.humidity, 1) + "," + 
-                  String(bme.gas_resistance / 1000.0, 1);
+                  String(bme.readTemperature(), 1) + "," + 
+                  String(bme.readPressure() / 100.0, 1) + "," + 
+                  String(bme.readHumidity(), 1);
     
     sendMessage(msg);
     lastSendTime = millis();            // timestamp the message
     LoRa.receive();
   }
+}
+
+void servoCommand(String command) {
+  int angle = command.substring(3).toInt();
+  // Validate angle
+  if (angle < 0 || angle > 180) {
+    Serial.println("Invalid servo angle");
+    sendMessage("SERVO: Invalid angle");
+    return;
+  }
+
+  if (command.startsWith("ail")) {
+    aileronServo.write(angle);
+  } else if (command.startsWith("rud")) {
+    rudderServo.write(angle);
+  } else {
+    Serial.print("Unknown servo: ");
+    Serial.println(command.substring(0, 2));
+
+    delay(10);
+    sendMessage("SERVO: Unknown");
+    return;
+  }
+
+  Serial.print("Servo ");
+  Serial.print(command.substring(0, 2));
+  Serial.println("set to: " + String(angle));
+
+  delay(10);
+  sendMessage("SERVO: " + command);
 }
 
 void sendMessage(String msg) {
@@ -104,18 +126,6 @@ void onReceive(int packetSize) {
 
   // Check for servo command
   if (msg.startsWith("SERVO:")) {
-    int angle = msg.substring(6).toInt();  // Get num after "SERVO:"
-    
-    // Validate angle
-    if (angle >= 0 && angle <= 180) {
-      myservo.write(angle);
-      Serial.println("Servo set to: " + String(angle));
-      delay(10);
-      sendMessage("SERVO: " + String(angle));
-    } else {
-      Serial.println("Invalid servo angle");
-      sendMessage("SERVO: Invalid angle");
-    }
-
+    servoCommand(msg.substring(6));
   }
 }
